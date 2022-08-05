05bf2d199ef011455dcbf7e74b9938899d4f88f0
vitor <vitor.azevedo009@gmail.com>
2022-08-05 16:18:24 -0300

    Adding docker files(not working yet) and backend configuration with poetry

---
+++ backend/backend/main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+def start():
+    """Launched with `poetry run start` at root level"""
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
+
