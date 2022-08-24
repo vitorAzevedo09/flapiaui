82e91274275c88e07d7c6e413dfdc8a19acd29c5
Vitor <vitor.azevedo009@gmail.com>
2022-08-24 14:37:08 -0300

    feature: Adding Monthly Books model

--- backend/app/models/monthly_payment.py
+++ backend/app/models/monthly_payment.py
@@ -2,30 +2,32 @@ from sqlalchemy import (
         Column,
         Integer,
         Boolean,
+        Numeric,
         ForeignKey,
         )
 
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql.sqltypes import TIMESTAMP
-from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 
 from configs.sessions import Base
 
 from uuid import uuid4
 
 
-class PaymentBook(Base):
-    ''' Carnê '''
-    __tablename__ = "payment_books"
+class MonthlyPayment(Base):
+    ''' Mensalidade '''
+    __tablename__ = "monthly_payments"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    year = Column(Integer, nullable=False)
+    price = Column(Numeric, nullable=False)
+    month = Column(Integer,nullable=False)
     is_payed = Column(Boolean, default=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False,
                         server_default=func.now())
-    payer_id = Column(Integer,
-                      ForeignKey("users.id", ondelete="CASCADE"),
-                      nullable=False)
-    payer = relationship("User", back_populates="payment_books")
-    monthly_payments = relationship("MonthlyPayment", back_populates="payment_book")
+    payment_book_id = Column(Integer,
+                             ForeignKey("payment_books.id",
+                                        ondelete="CASCADE"),
+                             nullable=False)
+    payment_book = relationship("PaymentBook", back_populates="monthly_payments")
