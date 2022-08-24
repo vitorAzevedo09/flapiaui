29c3dca5887ef4aa1d8937d05e5807073657facd
Vitor <vitor.azevedo009@gmail.com>
2022-08-24 17:34:21 -0300

    Make a base service and user service crud

---
+++ backend/app/services/user.py
@@ -0,0 +1,15 @@
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from models.user import User
+from schemas.user import UserCreate, UserUpdate
+from configs.sessions import get_session
+
+from base import BaseService
+
+class UserService(BaseService[User, UserCreate, UserUpdate]):
+    def __init__(self, db_session: Session):
+        super(UserService, self).__init__(User, db_session)
+
+def get_user_service(db_session: Session = Depends(get_session)) -> UserService:
+    return UserService(db_session)
