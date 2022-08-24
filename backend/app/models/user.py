ad84522a34b435da54507103a15c4bea43e5d056
Vitor <vitor.azevedo009@gmail.com>
2022-08-24 14:21:24 -0300

    feature: Replace primary key user for uuid

---
+++ backend/app/models/user.py
@@ -0,0 +1,36 @@
+from sqlalchemy import (
+        Column,
+        Boolean,
+        Integer,
+        String,
+        )
+
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from configs.sessions import Base
+
+from uuid import uuid4
+
+class User(Base):
+    ''' Usuario '''
+    __tablename__ = "users"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=True, unique=True)
+    document = Column(String, nullable=False, unique=True)
+    is_admin = Column(Boolean, default=False)
+    password = Column(String, nullable=False)
+    birth_date = Column(TIMESTAMP(timezone=True),
+                        nullable=False,
+                        server_default=func.now())
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False,
+                        server_default=func.now())
+    logged_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False,
+                        server_default=func.now())
+    payment_books = relationship("PaymentBook", back_populates="payer")
