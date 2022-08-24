eca82e5ab53335e1c188f461027ca394f8688781
Vitor <vitor.azevedo009@gmail.com>
2022-08-24 16:47:21 -0300

    feature: Adding base crud service class methods create

--- backend/app/services/base.py
+++ backend/app/services/base.py
@@ -1,11 +1,11 @@
 from typing import Any, Generic, List, Optional, Type, TypeVar
 
-import sqlalchemy
+from sqlalchemy.exc import IntegrityError
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette.exceptions import HTTPException
 
-from configs.models import Base
+from configs.sessions import Base
 
 ModelType = TypeVar("ModelType", bound=Base)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
@@ -15,3 +15,26 @@ class BaseService(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
     def __init__(self, model: Type[ModelType], db_session: Session):
         self.model = model
         self.db_session = db_session
+
+    def get(self, id: Any) -> Optional[ModelType]:
+        obj: Optional[ModelType] = self.db_session.query(self.model).get(id)
+        if obj is None:
+            raise HTTPException(status_code=404, detail="Not Found")
+        return obj
+
+    def list(self) -> List[ModelType]:
+        objs: List[ModelType] = self.db_session.query(self.model).all()
+        return objs
+
+    def create(self, obj: CreateSchemaType) -> ModelType:
+        db_obj: ModelType = self.model(**obj.dict())
+        self.db_session.add(db_obj)
+        try:
+            self.db_session.commit()
+        except IntegrityError as e:
+            self.db_session.rollback()
+            if "duplicate key" in str(e):
+                raise HTTPException(status_code=409, detail="Conflict Error")
+            else:
+                raise e
+        return db_obj
