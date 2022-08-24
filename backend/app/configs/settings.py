71f812c80352278d9556201036e1201a67182768
Vitor <vitor.azevedo009@gmail.com>
2022-08-24 13:37:46 -0300

    feature: Adding files settings.py and sessions.py

---
+++ backend/app/configs/settings.py
@@ -0,0 +1,11 @@
+from functools import lru_cache
+
+from pydantic import BaseSettings, PostgresDsn
+
+class Settings(BaseSettings):
+    database_url: PostgresDsn
+
+@lru_cache
+def get_settings() -> Settings:
+    settings =  Settings()
+    return settings
